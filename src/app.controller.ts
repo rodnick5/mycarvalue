5de69e1081f89a250ea44d4e503a4d3d5ac29769
Rodnick <crayz512yt@gmail.com>
2023-10-15 00:24:55 +0300

    updated app controller file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { CurrentUser } from './users/decorators/current-user.decorator';
-import { User } from './users/user.entity';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('whoami')
-  getHello(@CurrentUser() user: User) {
-    return user;
+  @Get()
+  getHello() {
+    return this.appService.getHello();
   }
 }
